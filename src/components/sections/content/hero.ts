e2ccaa5340df7a9ac98b27005435752367b9bb6a
Akash <akash.kumar@outsmarttechnology.com>
2026-05-27 22:23:29 +0530

    16

---
+++ src/components/sections/content/hero.ts
@@ -0,0 +1,153 @@
+/** Auto-generated from original HTML – do not edit manually unless syncing from source */
+export const HERO_HTML = `<div class="elementor-element elementor-element-ac92a47 e-flex e-con-boxed e-con e-parent" data-id="ac92a47" data-element_type="container" data-e-type="container" data-settings="{&quot;background_background&quot;:&quot;classic&quot;}">
+					<div class="e-con-inner">
+		<div class="elementor-element elementor-element-437f651 bg-section e-flex e-con-boxed e-con e-child" data-id="437f651" data-element_type="container" data-e-type="container" data-settings="{&quot;background_background&quot;:&quot;classic&quot;}">
+					<div class="e-con-inner">
+		<div class="elementor-element elementor-element-7901a32 e-con-full e-flex e-con e-child" data-id="7901a32" data-element_type="container" data-e-type="container">
+				<div class="elementor-element elementor-element-99458b4 section-title at-heading-animation at-animation-heading-none elementor-invisible elementor-widget elementor-widget-heading" data-id="99458b4" data-element_type="widget" data-e-type="widget" data-settings="{&quot;_animation&quot;:&quot;fadeInUp&quot;,&quot;ekit_we_effect_on&quot;:&quot;none&quot;}" data-widget_type="heading.default">
+				<div class="elementor-widget-container">
+					<h3 class="elementor-heading-title elementor-size-default">Your Health Our Priority</h3>				</div>
+				</div>
+				<div class="elementor-element elementor-element-60bb770 elementor-widget-mobile__width-inherit at-heading-animation at-animation-heading-style-3 elementor-widget elementor-widget-heading" data-id="60bb770" data-element_type="widget" data-e-type="widget" data-settings="{&quot;ekit_we_effect_on&quot;:&quot;none&quot;}" data-widget_type="heading.default">
+				<div class="elementor-widget-container">
+					<h1 class="elementor-heading-title elementor-size-default">Expert medical care you can rely on</h1>				</div>
+				</div>
+				<div class="elementor-element elementor-element-5e52f94 elementor-invisible elementor-widget elementor-widget-text-editor" data-id="5e52f94" data-element_type="widget" data-e-type="widget" data-settings="{&quot;_animation&quot;:&quot;fadeInUp&quot;,&quot;_animation_delay&quot;:100,&quot;ekit_we_effect_on&quot;:&quot;none&quot;}" data-widget_type="text-editor.default">
+				<div class="elementor-widget-container">
+									<p>Experience healthcare you can trust. Our dedicated team provides compassionate, high-quality care.</p>								</div>
+				</div>
+		<div class="elementor-element elementor-element-c37853f e-con-full e-flex elementor-invisible e-con e-child" data-id="c37853f" data-element_type="container" data-e-type="container" data-settings="{&quot;animation&quot;:&quot;fadeInUp&quot;,&quot;animation_delay&quot;:200}">
+				<div class="elementor-element elementor-element-0c2dd12 elementor-widget elementor-widget-button" data-id="0c2dd12" data-element_type="widget" data-e-type="widget" data-settings="{&quot;ekit_we_effect_on&quot;:&quot;none&quot;}" data-widget_type="button.default">
+				<div class="elementor-widget-container">
+									<div class="elementor-button-wrapper">
+					<a class="elementor-button elementor-button-link elementor-size-sm" href="/appointment/">
+						<span class="elementor-button-content-wrapper">
+									<span class="elementor-button-text">Book A Appointment</span>
+					</span>
+					</a>
+				</div>
+								</div>
+				</div>
+				<div class="elementor-element elementor-element-8d1b748 elementor-widget elementor-widget-button" data-id="8d1b748" data-element_type="widget" data-e-type="widget" data-settings="{&quot;ekit_we_effect_on&quot;:&quot;none&quot;}" data-widget_type="button.default">
+				<div class="elementor-widget-container">
+									<div class="elementor-button-wrapper">
+					<a class="elementor-button elementor-button-link elementor-size-sm" href="/about-us/">
+						<span class="elementor-button-content-wrapper">
+									<span class="elementor-button-text">About Us</span>
+					</span>
+					</a>
+				</div>
+								</div>
+				</div>
+				</div>
+		<div class="elementor-element elementor-element-f107e8e e-con-full e-flex elementor-invisible e-con e-child" data-id="f107e8e" data-element_type="container" data-e-type="container" data-settings="{&quot;animation&quot;:&quot;fadeInUp&quot;,&quot;animation_delay&quot;:300}">
+				<div class="elementor-element elementor-element-01d656f google-rating  at-heading-animation at-animation-heading-none elementor-widget elementor-widget-heading" data-id="01d656f" data-element_type="widget" data-e-type="widget" data-settings="{&quot;ekit_we_effect_on&quot;:&quot;none&quot;}" data-widget_type="heading.default">
+				<div class="elementor-widget-container">
+					<h2 class="elementor-heading-title elementor-size-default">Google Rating <span>5.0</span></h2>				</div>
+				</div>
+				<div class="elementor-element elementor-element-632c692 elementor-widget elementor-widget-rating" data-id="632c692" data-element_type="widget" data-e-type="widget" data-settings="{&quot;ekit_we_effect_on&quot;:&quot;none&quot;}" data-widget_type="rating.default">
+				<div class="elementor-widget-container">
+							<div class="e-rating" itemtype="https://schema.org/Rating" itemscope="" itemprop="reviewRating">
+			<meta itemprop="worstRating" content="0">
+			<meta itemprop="bestRating" content="5">
+			<div class="e-rating-wrapper" itemprop="ratingValue" content="5" role="img" aria-label="Rated 5 out of 5">
+							<div class="e-icon">
+				<div class="e-icon-wrapper e-icon-marked">
+					<i aria-hidden="true" class="eicon-star"></i>				</div>
+				<div class="e-icon-wrapper e-icon-unmarked">
+					<i aria-hidden="true" class="eicon-star"></i>				</div>
+			</div>
+						<div class="e-icon">
+				<div class="e-icon-wrapper e-icon-marked">
+					<i aria-hidden="true" class="eicon-star"></i>				</div>
+				<div class="e-icon-wrapper e-icon-unmarked">
+					<i aria-hidden="true" class="eicon-star"></i>				</div>
+			</div>
+						<div class="e-icon">
+				<div class="e-icon-wrapper e-icon-marked">
+					<i aria-hidden="true" class="eicon-star"></i>				</div>
+				<div class="e-icon-wrapper e-icon-unmarked">
+					<i aria-hidden="true" class="eicon-star"></i>				</div>
+			</div>
+						<div class="e-icon">
+				<div class="e-icon-wrapper e-icon-marked">
+					<i aria-hidden="true" class="eicon-star"></i>				</div>
+				<div class="e-icon-wrapper e-icon-unmarked">
+					<i aria-hidden="true" class="eicon-star"></i>				</div>
+			</div>
+						<div class="e-icon">
+				<div class="e-icon-wrapper e-icon-marked">
+					<i aria-hidden="true" class="eicon-star"></i>				</div>
+				<div class="e-icon-wrapper e-icon-unmarked">
+					<i aria-hidden="true" class="eicon-star"></i>				</div>
+			</div>
+						</div>
+		</div>
+						</div>
+				</div>
+				<div class="elementor-element elementor-element-64faf60 google-rating  at-heading-animation at-animation-heading-none elementor-widget elementor-widget-heading" data-id="64faf60" data-element_type="widget" data-e-type="widget" data-settings="{&quot;ekit_we_effect_on&quot;:&quot;none&quot;}" data-widget_type="heading.default">
+				<div class="elementor-widget-container">
+					<h2 class="elementor-heading-title elementor-size-default">Based On 500 Reviews</h2>				</div>
+				</div>
+				</div>
+				</div>
+		<div class="elementor-element elementor-element-ca00e39 e-con-full e-flex e-con e-child" data-id="ca00e39" data-element_type="container" data-e-type="container" data-settings="{&quot;background_background&quot;:&quot;classic&quot;}">
+				<div class="elementor-element elementor-element-010f2f5 hero-section-image at-image-animation at-animation-image-none elementor-widget elementor-widget-image" data-id="010f2f5" data-element_type="widget" data-e-type="widget" data-settings="{&quot;ekit_we_effect_on&quot;:&quot;none&quot;}" data-widget_type="image.default">
+				<div class="elementor-widget-container">
+															<img fetchpriority="high" decoding="async" width="513" height="806" src="/wp-content/uploads/2025/08/hero-img.png" class="attachment-full size-full wp-image-11038" alt="" srcset="/wp-content/uploads/2025/08/hero-img.png 513w, /wp-content/uploads/2025/08/hero-img-191x300.png 191w" sizes="(max-width: 513px) 100vw, 513px" />															</div>
+				</div>
+		<div class="elementor-element elementor-element-529476f e-con-full excerpt-doctor-box e-flex e-con e-child" data-id="529476f" data-element_type="container" data-e-type="container" data-settings="{&quot;background_background&quot;:&quot;classic&quot;,&quot;position&quot;:&quot;absolute&quot;}">
+		<div class="elementor-element elementor-element-8355328 e-con-full e-flex e-con e-child" data-id="8355328" data-element_type="container" data-e-type="container">
+				<div class="elementor-element elementor-element-1a861bf image-anime excerpt-doctor-image at-image-animation at-animation-image-none elementor-widget elementor-widget-image" data-id="1a861bf" data-element_type="widget" data-e-type="widget" data-settings="{&quot;ekit_we_effect_on&quot;:&quot;none&quot;}" data-widget_type="image.default">
+				<div class="elementor-widget-container">
+															<img decoding="async" width="44" height="44" src="/wp-content/uploads/2025/01/excerpt-doctor-img-1.jpg" class="attachment-full size-full wp-image-10830" alt="" />															</div>
+				</div>
+				<div class="elementor-element elementor-element-9bc43e5 image-anime excerpt-doctor-image at-image-animation at-animation-image-none elementor-widget elementor-widget-image" data-id="9bc43e5" data-element_type="widget" data-e-type="widget" data-settings="{&quot;ekit_we_effect_on&quot;:&quot;none&quot;}" data-widget_type="image.default">
+				<div class="elementor-widget-container">
+															<img decoding="async" width="45" height="44" src="/wp-content/uploads/2025/01/excerpt-doctor-img-2.jpg" class="attachment-full size-full wp-image-10829" alt="" />															</div>
+				</div>
+				<div class="elementor-element elementor-element-997fb30 image-anime excerpt-doctor-image at-image-animation at-animation-image-none elementor-widget elementor-widget-image" data-id="997fb30" data-element_type="widget" data-e-type="widget" data-settings="{&quot;ekit_we_effect_on&quot;:&quot;none&quot;}" data-widget_type="image.default">
+				<div class="elementor-widget-container">
+															<img loading="lazy" decoding="async" width="44" height="44" src="/wp-content/uploads/2025/01/excerpt-doctor-img-3.jpg" class="attachment-full size-full wp-image-10828" alt="" />															</div>
+				</div>
+				<div class="elementor-element elementor-element-f598a32 image-anime excerpt-doctor-image at-image-animation at-animation-image-none elementor-widget elementor-widget-image" data-id="f598a32" data-element_type="widget" data-e-type="widget" data-settings="{&quot;ekit_we_effect_on&quot;:&quot;none&quot;}" data-widget_type="image.default">
+				<div class="elementor-widget-container">
+															<img loading="lazy" decoding="async" width="44" height="44" src="/wp-content/uploads/2025/01/excerpt-doctor-img-4.jpg" class="attachment-full size-full wp-image-10827" alt="" />															</div>
+				</div>
+				</div>
+		<div class="elementor-element elementor-element-d6f7d1d e-con-full e-flex e-con e-child" data-id="d6f7d1d" data-element_type="container" data-e-type="container">
+				<div class="elementor-element elementor-element-6a47206 elementor-widget elementor-widget-text-editor" data-id="6a47206" data-element_type="widget" data-e-type="widget" data-settings="{&quot;ekit_we_effect_on&quot;:&quot;none&quot;}" data-widget_type="text-editor.default">
+				<div class="elementor-widget-container">
+									<p>Talk to our 48+ Doctors</p>								</div>
+				</div>
+				</div>
+				</div>
+				<div class="elementor-element elementor-element-b987fa1 elementor-absolute satisfied-clients-box elementor-widget elementor-widget-elementskit-funfact" data-id="b987fa1" data-element_type="widget" data-e-type="widget" data-settings="{&quot;_position&quot;:&quot;absolute&quot;,&quot;ekit_we_effect_on&quot;:&quot;none&quot;}" data-widget_type="elementskit-funfact.default">
+				<div class="elementor-widget-container">
+					<div class="ekit-wid-con" >
+		<div class="elementskit-funfact text-left     ">
+			
+			<div class="elementskit-funfact-inner position_left">
+									<div class="funfact-icon">
+						<svg xmlns="http://www.w3.org/2000/svg" width="26" height="22" viewBox="0 0 26 22" fill="none"><path d="M15.4517 4.46843C16.6076 5.19319 17.4181 6.41964 17.5657 7.84326C18.0371 8.06348 18.5604 8.19018 19.1151 8.19018C21.1402 8.19018 22.7816 6.5488 22.7816 4.52397C22.7816 2.49882 21.1402 0.857422 19.1151 0.857422C17.1093 0.858052 15.4822 2.47012 15.4517 4.46843ZM13.0496 11.9756C15.0748 11.9756 16.7162 10.3339 16.7162 8.30908C16.7162 6.28423 15.0745 4.64284 13.0496 4.64284C11.0248 4.64284 9.38246 6.28454 9.38246 8.30938C9.38246 10.3342 11.0248 11.9756 13.0496 11.9756ZM14.6049 12.2255H11.4937C8.90511 12.2255 6.79915 14.3318 6.79915 16.9204V20.7252L6.80883 20.7848L7.0709 20.8668C9.54126 21.6387 11.6875 21.8961 13.454 21.8961C16.9043 21.8961 18.9042 20.9124 19.0274 20.8496L19.2723 20.7258H19.2985V16.9204C19.2995 14.3318 17.1935 12.2255 14.6049 12.2255ZM20.671 8.44038H17.5838C17.5504 9.67558 17.0232 10.7879 16.1892 11.5878C18.4902 12.272 20.1737 14.4057 20.1737 16.926V18.0985C23.2218 17.9868 24.9784 17.1229 25.0941 17.0649L25.339 16.9407H25.3652V13.1347C25.3652 10.5464 23.2593 8.44038 20.671 8.44038ZM6.61601 8.19078C7.33329 8.19078 8.00064 7.98147 8.56597 7.62486C8.74568 6.45271 9.37403 5.42843 10.2716 4.73082C10.2754 4.66218 10.2819 4.59416 10.2819 4.5249C10.2819 2.49976 8.64023 0.858362 6.61601 0.858362C4.59056 0.858362 2.94947 2.49976 2.94947 4.5249C2.94947 6.54911 4.59056 8.19078 6.61601 8.19078ZM9.90879 11.5878C9.07889 10.7919 8.55349 9.68558 8.51512 8.45788C8.40061 8.44948 8.28736 8.44038 8.17068 8.44038H5.05979C2.47119 8.44038 0.365234 10.5464 0.365234 13.1347V16.9401L0.374906 16.9987L0.63698 17.0814C2.61876 17.7001 4.38745 17.9852 5.9237 18.0751V16.926C5.92433 14.4057 7.60722 12.2726 9.90879 11.5878Z" fill="currentColor"></path></svg>					</div>
+				
+				<div class="funfact-content">
+					<div class="number-percentage-wraper">
+												<span class="number-percentage"
+						      data-value="3500"
+						      data-animation-duration="3500"
+							  data-style="static">0</span>
+						+											</div>
+
+					<h3 class="funfact-title">satisfied clients</h3>								</div>
+		</div>
+		</div>
+
+
+		</div>				</div>
+				</div>
+				</div>
+					</div>
+				</div>
+					</div>
+				</div>`;
